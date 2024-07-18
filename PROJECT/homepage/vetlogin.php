c0290acc0617cca19548d773b429965a7bf34b81
khalif-abdullahi <abdullahi.khalif@strathmore.edu>
2024-07-18 11:52:51 +0300

    wayne

--- PROJECT/homepage/vetlogin.php
+++ PROJECT/homepage/vetlogin.php
@@ -1,6 +1,24 @@
 <?php
 
-include_once('db.php');
+if ($_SERVER['REQUEST_METHOD']=='POST'){
+    include 'db.php';
+    $email = $_POST['email'];
+    $password = $_POST['password'];
 
+    $sql = "SELECT * FROM vetregister WHERE email = '$email' AND password = '$password'";
 
+    $result=mysqli_query($conn,$sql);
+    if($result){
+        $num=mysqli_num_rows($result);
+        if($num>0){
+            echo "login successfully";
+            session_start();
+            $_SESSION['email']=$email;
+            header('location: vetaccount.html');
+        }else{
+            echo "Wrong Credentials";
+            header('location:login.html');
+        }
+    }
+}
 ?>
